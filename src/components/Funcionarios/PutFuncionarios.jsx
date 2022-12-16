e7a9d05e56b1814b282e4ed72e37163f37855dd9
jessicarsmos <jessicacosima@gmail.com>
2022-12-15 22:35:26 -0300

    added quartos e funcionarios

--- src/components/Funcionarios/PutFuncionarios.jsx
+++ src/components/Funcionarios/PutFuncionarios.jsx
@@ -1,40 +1,97 @@
-import React from 'react'
+import styles from './Put.module.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function PutFuncionarios() {
+
+  let [funcionario] = useState([]);
+
+  function Selects(ar) {
+    ar.unshift("Selecione o nome do funcionário")
+    $genero.innerHTML = ""
+    ar.forEach((e, i) => {
+      let op = document.createElement("option")
+      if (i == 0) {
+        op.setAttribute("disabled", 'true');
+        op.setAttribute("selected", 'true');
+      }
+      op.value = e
+      op.innerHTML = e
+      $genero.appendChild(op)
+    })
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/funcionarios").then(e => {
+      let nomes = []
+      e.data.map(el => nomes.push(el.nome))
+      Selects(nomes)
+    })
+  }, [])
+
+  useEffect(() => {
+    Selects(funcionario)
+  }, [funcionario])
+
+  function mudar() {
+    axios.get("http://localhost:3000/funcionarios?nome=" + $genero.value).then(elemento => {
+      let obj = elemento.data[0]
+      $nome.value = obj.nome
+      $usuario.value = obj.usuario
+      $cpf.value = obj.cpf
+      $turno.value = obj.turno
+      $funcao.value = obj.funcao
+      $btclick.sid = obj.id
+      
+
+    })
+  }
+
+  function Atualizar() {
+
+    let obj = {
+      nome: $nome.value,
+      usuario: $usuario.value,
+      cpf: $cpf.value,
+      turno: $turno.value,
+      funcao: $funcao.value,
+      genero: $genero.value,
+    }
+
+    axios.put("http://localhost:3000/funcionarios/" + $btclick.sid, obj);
+
+    alert('Funcionário atualizado com sucesso!')
+
+    window.location.reload()
+
+  }
+
   return (
-    <section>
+    <section className={styles.putFunc}>
       <h1>FUNCIONÁRIOS</h1>
       <p>SELECIONE O FUNCIONÁRIO A SER ATUALIZADO</p>
     <form>      
-      <select name="" id="">
-        <option value=""></option>
+      <select id="$genero" onChange={mudar}>
+        <option value="Carlos Alberto">Carlos Alberto</option>
       </select>
             
-      <label htmlFor='nome'>
+      <label>
          Nome
-        <input id='nome' name='nome' type='text' />
+        <input id='$nome' name='nome' type='text' />
       </label>
       <label>Usuário
-        <input type='text' />
+        <input id='$usuario' type='text' />
       </label>
        <label>CPF
-        <input type='text' />
-      </label>
-      <label>Turno
-        <select name="turno">
-          <option value="manha">Manhã</option>
-          <option value="tarde">Tarde</option>
-          <option value="noite">Noite</option>
-        </select>
+        <input id='$cpf' type='text' />
       </label>
       <label>Função
-        <select name="funcao">
-          <option value="manha">Manhã</option>
-          <option value="tarde">Tarde</option>
-          <option value="noite">Noite</option>
-        </select>
+      <input id='$funcao' type='text' />
       </label>
-      <button>Atualizar</button>
+      <label>Turno
+      <input id='$turno' type='text' />
+      </label>
+      <button id="$btclick" onClick={Atualizar}>Atualizar</button>
     </form>
     </section>
    
--- src/components/Quartos/PutQuartos.jsx
+++ src/components/Quartos/PutQuartos.jsx
@@ -1,24 +1,84 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from './Put.module.css'
 
 function PutQuartos() {
+
+  let [quartos] = useState([]);
+
+  function Selects(ar) {
+    ar.unshift("Selecione o quarto")
+    $genero.innerHTML = ""
+    ar.forEach((e, i) => {
+      let op = document.createElement("option")
+      if (i == 0) {
+        op.setAttribute("disabled", 'true');
+        op.setAttribute("selected", 'true');
+      }
+      op.value = e
+      op.innerHTML = e
+      $genero.appendChild(op)
+    })
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/quartos").then(e => {
+      let nomes = []
+      e.data.map(el => nomes.push(el.nome))
+      Selects(nomes)
+    })
+  }, [])
+
+  useEffect(() => {
+    Selects(quartos)
+  }, [quartos])
+
+  function mudar() {
+    axios.get("http://localhost:3000/quartos?nome=" + $genero.value).then(elemento => {
+      let obj = elemento.data[0]
+      $nome.value = obj.nome
+      $descricao.value = obj.descricao
+      $btclick.sid = obj.id
+      
+
+    })
+  }
+
+  function Atualizar() {
+
+    let obj = {
+      nome: $nome.value,
+      descricao: $descricao.value,
+      genero: $genero.value,
+    }
+
+    axios.put("http://localhost:3000/quartos/" + $btclick.sid, obj);
+
+    alert('Quarto atualizado com sucesso!')
+
+    window.location.reload()
+
+  }
+
   return (
-    <section>
+    <section className={styles.formQuartos}>
       <h1>QUARTOS</h1>
       <p>SELECIONE QUARTO A SER ATUALIZADO</p>
     <form>      
-      <select name="" id="">
-        <option value=""></option>
+      <select id="$genero" onChange={mudar}>
+        <option value="suite bela vista">suite bela vista</option>
       </select>
             
-      <label htmlFor='nome'>
+      <label>
          Nome do quarto
-        <input id='nome' name='nome' type='text' />
+        <input id='$nome' type='text' />
       </label>
       <label>
         Descrição
-        <input type='text' />
+        <input id='$descricao' type='text' />
       </label>
-      <button>Atualizar</button>
+      <button id="$btclick" onClick={Atualizar}>Atualizar</button>
     </form>
     </section>
   )
