5c5f6d3fd3eae63065349e6388a13e1f821905b0
jessica <jessicacosima@gmail.com>
2022-12-19 08:12:24 -0300

    atualização final

--- src/components/Painel/Painel.jsx
+++ src/components/Painel/Painel.jsx
@@ -9,11 +9,7 @@ import 'aos/dist/aos.css'
 function Painel() {
 
    return (
-<<<<<<< HEAD
       <div className={styles.painel} style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: 'cover' }}>
-=======
-      <div className={styles.painel} style={{backgroundImage: `url(${background})`,height:"1094px",width:"1736" }}>
->>>>>>> 44f5b7e2663b9d5b1cbf46b452ca1738eb0ee026
          <section >
             <form>
                <h1 data-aos="zoom-in" data-aos-delay="300"> Seja bem vindo(a) ao seu painel <br /> de administrador </h1>
